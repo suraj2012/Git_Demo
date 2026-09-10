436c1e532df009b3bfa4f20066db3ae87e27c84f
Suraj <kawalesuraj34@gmail.com>
2026-09-10 20:55:45 +0530

    Changes in Employee Class added new method EmployeeDetails and call in Program.cs

--- Employee.cs
+++ Employee.cs
@@ -2,4 +2,9 @@ public class Employee{
   public void GetInfo(){
     Console.WriteLine("Employee Details");
   }
+
+    public void EmployeeDetails(String name, int age)
+    {
+        Console.WriteLine($"Employee: {name}, Age : {age}");
+    }
 }
